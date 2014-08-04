490a0b9123d32c2054ba92c880e3ed93099cf270
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-04 17:37:31 +0200

    init

---
+++ test.js
@@ -0,0 +1,15 @@
+'use strict';
+var test = require('ava');
+var numIsFinite = require('./');
+
+test(function (t) {
+	t.assert(numIsFinite(0));
+	t.assert(numIsFinite(100));
+	t.assert(numIsFinite(-100));
+	t.assert(numIsFinite(4e44));
+	t.assert(!numIsFinite('0'));
+	t.assert(!numIsFinite(NaN));
+	t.assert(!numIsFinite(undefined));
+	t.assert(!numIsFinite(Infinity));
+	t.assert(!numIsFinite(-Infinity));
+});
