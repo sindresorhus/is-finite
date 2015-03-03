107fd6cd803896fceddd2c1d308bdd4bda18dbf7
An Phan <an@construct.sg>
2015-03-03 12:16:04 +0800

    Shortened the function even a bit more

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 module.exports = Number.isFinite || function (val) {
 	// Number.isNaN() => val !== val
-	if (typeof val !== 'number' || val !== val || val === Infinity || val === -Infinity) {
-		return false;
-	}
-
-	return true;
+	return !(typeof val !== 'number' || val !== val || val === Infinity || val === -Infinity);
 };
