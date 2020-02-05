f980389c2f2eb84e4cec432b29008b19f75b9125
Agastya Chandrakant <acagastya@outlook.com>
2020-02-05 22:46:45 +0530

    preserving quotes

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const test = require("ava");
+const test = require('ava');
 Number.isFinite = undefined;
-const m = require("./");
+const m = require('./');
 
-test("isFinite tests", t => {
+test('isFinite tests', t => {
 	t.true(m(0));
 	t.true(m(100));
 	t.true(m(-100));
 	t.true(m(4e44));
-	t.false(m("0"));
+	t.false(m('0'));
 	t.false(m(NaN));
 	t.false(m(undefined));
 	t.false(m(Infinity));
