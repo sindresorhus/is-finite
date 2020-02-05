856cc2829d423d5dd8755b19e07978bcd6d92988
Agastya Chandrakant <me@hanabi.in>
2020-02-05 20:42:03 +0530

    Update test.js to work with ava@3.2.0 + require() style of import to work without babel

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-import test from 'ava';
-
+const test = require("ava");
 Number.isFinite = undefined;
-const m = require('./');
+const m = require("./");
 
-test(t => {
+test("isFinite tests", t => {
 	t.true(m(0));
 	t.true(m(100));
 	t.true(m(-100));
 	t.true(m(4e44));
-	t.false(m('0'));
+	t.false(m("0"));
 	t.false(m(NaN));
 	t.false(m(undefined));
 	t.false(m(Infinity));
